943d38f0ba0d44b9acd4df051f8e8c815ed2ef8b
Mr_bl <1300264929@qq.com>
2025-05-20 00:48:59 +0800

    update

--- src/main/java/com/mlc/mlc/sendmail.java
+++ src/main/java/com/mlc/mlc/sendmail.java
@@ -7,6 +7,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -22,47 +23,6 @@ import static com.mlc.mlc.Mlc.instance;
 import static org.bukkit.inventory.ItemStack.serializeItemsAsBytes;
 
 public class sendmail implements CommandExecutor {
-//保存到文件
-    public  void savetomailgui(ItemStack item,String player) throws IOException {
-        File mailDir = new File(instance.getDataFolder(), "mail");
-
-        if (!mailDir.exists()) mailDir.mkdirs(); // 创建目录
-//
-//        Player player1 = Bukkit.getPlayer(player);
-        String string = player+".yml";
-        File file = new File(mailDir,string);
-        if(!file.exists())
-        {
-            file.createNewFile();
-        };
-
-        FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
-        if (!fileConfiguration.contains("item")) {
-            fileConfiguration.set("item", new ArrayList<>());
-            fileConfiguration.save(file);
-        }
-
-
-        ItemMeta itemMeta = item.getItemMeta();
-
-        Map<String, Object> serialized = itemMeta.serialize();
-        String type = item.getType().toString();
-        int amount = item.getAmount();
-
-
-        @SuppressWarnings("unchecked")
-        List<Map<String, Object>> itemList = (List<Map<String, Object>>) fileConfiguration.get("item");
-
-        Map<String, Object> newItem = new HashMap<>();
-        newItem.put("type",type);
-        newItem.put("amount",amount);
-        newItem.put("itemmeta",serialized);
-
-        itemList.add(newItem);
-        fileConfiguration.set("item",itemList);
-        fileConfiguration.save(file);
-    }
-
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
@@ -93,5 +53,64 @@ public class sendmail implements CommandExecutor {
         return true;
     }
 
+
+    //保存到文件
+    public  void savetomailgui(ItemStack item,String player) throws IOException {
+        File mailDir = new File(instance.getDataFolder(), "mail");
+
+        // 初始化目录
+        if (!mailDir.exists()) mailDir.mkdirs();
+
+        //初始化文件
+        String string = player+".yml";
+        File file = new File(mailDir,string);
+        if(!file.exists())
+        {
+            file.createNewFile();
+        };
+
+        //初始化item
+        FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+        if (!fileConfiguration.contains("item")) {
+            fileConfiguration.set("item", null);
+            fileConfiguration.save(file);
+        }
+
+        //获取物品信息
+        ItemMeta itemMeta = item.getItemMeta();
+        //序列化itemmeta以及其他信息
+        Map<String, Object> serialized = itemMeta.serialize();
+        String type = item.getType().toString();
+        int amount = item.getAmount();
+
+        //获取空闲格子
+        int freeslot = getfreeslot(fileConfiguration);
+
+        @SuppressWarnings("unchecked")
+        List<Map<String, Object>> itemList = (List<Map<String, Object>>) fileConfiguration.get("item");
+
+        Map<String, Object> newItem = new HashMap<>();
+        newItem.put("type",type);
+        newItem.put("amount",amount);
+        newItem.put("itemmeta",serialized);
+
+        itemList.add(newItem);
+        fileConfiguration.set("item",itemList);
+        fileConfiguration.save(file);
+    }
+
+    public int getfreeslot (FileConfiguration fileConfiguration)
+    {
+        ConfigurationSection configurationSection = fileConfiguration.getConfigurationSection("item");
+        if(configurationSection==null)
+        {
+            return 0;
+        }
+        Map<String,Object> items = configurationSection.getValues(false);
+        for(var solts : configurationSection.getKeys(false)){
+
+        }
+        return -1;
+    };
 
 }
