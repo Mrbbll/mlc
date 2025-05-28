b73f62bee1e3082f52ba87b57d6378c51727d157
Mr_bl <1300264929@qq.com>
2025-05-28 13:24:43 +0800

    update

---
+++ src/main/java/com/mlc/mlc/guis/PDCloader.java
@@ -0,0 +1,41 @@
+package com.mlc.mlc.guis;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mlc.mlc.Mlc.armortype;
+import static com.mlc.mlc.Mlc.damagetype;
+
+
+public class PDCloader {
+    public static void loadpdc(ConfigurationSection configurationSection, ItemMeta meta){
+        ConfigurationSection section = configurationSection.getConfigurationSection("tagged");
+        if (section != null) {
+            for(String type : section.getKeys(false)){
+                PersistentDataContainer persistentDataContainer = meta.getPersistentDataContainer();
+                if(type.equals("armortype")){
+                    persistentDataContainer.set(armortype, PersistentDataType.STRING, section.getString("armortype","entity"));
+                    List<Component> lore = meta.lore();
+                    if (lore != null) {
+                        lore.addLast(Component.text("防御类型："+section.getString("armortype","entity")));
+                    }
+                    meta.lore(lore);
+                }
+                if(type.equals("damagetype")){
+                    persistentDataContainer.set(damagetype, PersistentDataType.STRING, section.getString("damagetype","entity"));
+                    List<Component> lore = meta.lore();
+                    if (lore != null) {
+                        lore.addLast(Component.text("攻击类型："+section.getString("damagetype","entity")));
+                    }
+                    meta.lore(lore);
+                }
+            }
+        }
+    }
+}
