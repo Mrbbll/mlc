9c65a8f06ba1cf97b1c92318b26e004fb4631aa5
Mr_bl <1300264929@qq.com>
2025-07-07 21:54:50 +0800

    update

--- src/main/java/com/mlc/mlc/commands/tpahere.java
+++ src/main/java/com/mlc/mlc/commands/tpahere.java
@@ -40,6 +40,7 @@ public class tpahere implements CommandExecutor {
         target.sendMessage(Component.text(player.getName() + " 想要你传送到ta那里。",TextColor.fromHexString("#e5fe67")));
         target.sendMessage(Component.text("使用 /tpaccept 接受，3分钟后自动拒绝",TextColor.fromHexString("#e5fe67")));
         target.sendMessage(Component.text("或者点我同意", TextColor.fromHexString("#e5fe67"))
+                        .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/tpaccept"))
                         .append(Component.text("[✓]", TextColor.fromHexString("#3cff2e"), TextDecoration.BOLD)
                                 .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/tpaccept")))
                 );
