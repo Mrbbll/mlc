8e750d1a43fd02a6baff645c3d2552bd8eb0d39a
Mr_bl <1300264929@qq.com>
2026-02-18 09:40:43 +0800

    update

--- src/main/java/com/mlc/mlc/items/loader/Attributeloader.java
+++ src/main/java/com/mlc/mlc/items/loader/Attributeloader.java
@@ -6,6 +6,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Objects;
@@ -15,17 +16,33 @@ public class Attributeloader {
 
     public static void applyAttributes(ItemMeta meta, ConfigurationSection section) {
         ConfigurationSection configurationSection = section.getConfigurationSection("attribute_modifiers");
-        if(configurationSection!=null){
-            for (String attributeName : configurationSection.getKeys(false)) {
-                long timeMillis = System.currentTimeMillis();
-                double value = configurationSection.getDouble(attributeName, 0);
-                AttributeModifier attributeModifier = new AttributeModifier(Objects.requireNonNull(NamespacedKey.fromString("mlc:"+timeMillis)),value,AttributeModifier.Operation.ADD_NUMBER);
-                Attribute attribute = RegistryAccess.registryAccess().getRegistry(RegistryKey.ATTRIBUTE).get(NamespacedKey.minecraft(attributeName.toLowerCase()));
-                if (attribute != null) {
-                    meta.addAttributeModifier(attribute,attributeModifier);
+//        attribute_modifiers:
+//          - ADD_NUMBER:
+//              - ANY:
+//                  ATTACK_DAMAGE: 19
+        if (configurationSection != null) {
+            //操作类型
+            for (String operationName : configurationSection.getKeys(false)) {
+                ConfigurationSection operationSection = configurationSection.getConfigurationSection(operationName);
+                if (operationSection != null) {
+                    //生效栏位
+                    for (String slotName : operationSection.getKeys(false)) {
+                        ConfigurationSection slotSection = operationSection.getConfigurationSection(slotName);
+                        if (slotSection != null) {
+                            //属性
+                            for (String attributeName : slotSection.getKeys(false)) {
+                                long timeMillis = System.currentTimeMillis();
+                                double value = slotSection.getDouble(attributeName, 0);
+                                AttributeModifier attributeModifier = new AttributeModifier(Objects.requireNonNull(NamespacedKey.fromString("mlc:" + timeMillis)), value, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.ANY);
+                                Attribute attribute = RegistryAccess.registryAccess().getRegistry(RegistryKey.ATTRIBUTE).get(NamespacedKey.minecraft(attributeName.toLowerCase()));
+                                if (attribute != null) {
+                                    meta.addAttributeModifier(attribute, attributeModifier);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-
     }
 }
