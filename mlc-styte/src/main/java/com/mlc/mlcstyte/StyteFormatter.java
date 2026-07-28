4e0fc58c5681f1b5e14f69d8b321f87d650d309e
Mr_bl <1300264929@qq.com>
2026-07-28 21:12:41 +0800

    update

--- mlc-styte/src/main/java/com/mlc/mlcstyte/StyteFormatter.java
+++ mlc-styte/src/main/java/com/mlc/mlcstyte/StyteFormatter.java
@@ -74,6 +74,7 @@ public final class StyteFormatter {
             if (!before.isEmpty()) {
                 builder.append(Component.text(before));
             }
+
             // Append the replacement Component for the placeholder
             String placeholder = matcher.group();
             switch (placeholder) {
